07d1636fdf04916170fc99905d5841be233a8e5b
Raphaël Vinot <raphael@vinot.info>
2019-07-05 15:44:52 +0200

    fix: Properly pass OS, Browser, and User-Agent when scraping.

---
+++ website/web/static/scrape.js
@@ -0,0 +1,26 @@
+$("#os").change(function(){
+  var os_name = $(this).find(":selected").val().replace(/(:|\.|\[|\]|,|=)/g, "\\$1").replace(/ /g,"_");
+  var first_browser_name = $("[id='" + os_name + "']").find('select option:first-child').val().replace(/(:|\.|\[|\]|,|=)/g, "\\$1").replace(/ /g,"_");
+  // Hide and disable everything
+  $(".style-sub-1").hide();
+  $(".style-sub-1 > div > select").attr('disabled', true);
+  $(".style-sub-2").hide();
+  $(".style-sub-2 > div > select").attr('disabled', true);
+
+  $("[id='" + os_name + "']").show();
+  $("[id='" + os_name + "'] > div > select").removeAttr('disabled');
+  $("[id='" + os_name + '_' + first_browser_name + "']").show();
+  $("[id='" + os_name + '_' + first_browser_name + "'] > div > select").removeAttr('disabled');
+});
+
+$('select[name="browser"]').change(function(){
+  var browser_name = $(this).find(":selected").val().replace(/(:|\.|\[|\]|,|=)/g, "\\$1").replace(/ /g,"_");
+  var os_name = $(this).parent().parent().attr("id").replace(/(:|\.|\[|\]|,|=)/g, "\\$1").replace(/ /g,"_");
+
+  // Hide what makes sense
+  $(".style-sub-2").hide();
+  $(".style-sub-2 > div > select").attr('disabled', true);
+
+  $("[id='" + os_name + '_' + browser_name + "']").show();
+  $("[id='" + os_name + '_' + browser_name + "'] > div > select").removeAttr('disabled');
+});
