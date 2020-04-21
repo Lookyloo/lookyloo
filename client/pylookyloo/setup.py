d3902a05052a4e22620798b17dd894f06c9554b9
Raphaël Vinot <raphael@vinot.info>
2020-04-21 18:55:59 +0200

    chg: readd setup file

---
+++ client/pylookyloo/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+setup(
+    name='pylookyloo',
+    version='0.1',
+    author='Raphaël Vinot',
+    author_email='raphael.vinot@circl.lu',
+    maintainer='Raphaël Vinot',
+    url='https://github.com/CIRCL/lookyloo/client',
+    description='Python client for Lookyloo',
+    packages=['pylookyloo'],
+    scripts=['bin/lookyloo'],
+    install_requires=['requests'],
+    classifiers=[
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Operating System :: POSIX :: Linux',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Telecommunications Industry',
+        'Intended Audience :: Information Technology',
+        'Programming Language :: Python :: 3',
+        'Topic :: Security',
+        'Topic :: Internet',
+    ]
+)
