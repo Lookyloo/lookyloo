b0d761df8ed669f256797f9fc8e457fd65759471
Raphaël Vinot <raphael@vinot.info>
2020-09-22 13:00:28 +0200

    chg: Remove FileSaver.js from the deps, bump datatables

--- website/3rdparty.sh
+++ website/3rdparty.sh
@@ -7,11 +7,7 @@ mkdir -p web/static/
 
 wget -q https://d3js.org/d3.v5.min.js -O web/static/d3.v5.min.js
 
-FileSaver="v2.0.2"
-
-wget -q https://raw.githubusercontent.com/eligrey/FileSaver.js/${FileSaver}/src/FileSaver.js -O web/static/FileSaver.js
-
-datatables="1.10.21"
+datatables="1.10.22"
 
 wget -q https://cdn.datatables.net/v/bs4/dt-${datatables}/datatables.min.css -O web/static/datatables.min.css
 wget -q https://cdn.datatables.net/v/bs4/dt-${datatables}/datatables.min.js -O web/static/datatables.min.js
