542d6477f9a654f6b1f0528ad7eca4daf2e02a92
Raphaël Vinot <raphael@vinot.info>
2020-09-25 19:02:40 +0200

    chg: Add missing js

---
+++ website/web/static/generic.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// Copy to clipboard
+// Source: https://codepen.io/nathanlong/pen/ZpAmjv
+let copyToClipboard = (text, el) => {
+  const elOriginalText = el.attr('data-original-title');
+
+  const copyTextArea = document.createElement("textarea");
+  copyTextArea.value = text;
+  document.body.appendChild(copyTextArea);
+  copyTextArea.select();
+
+  const successful = document.execCommand('copy');
+  const msg = successful ? 'Copied!' : 'Whoops, not copied!';
+  el.attr('data-original-title', msg).tooltip('show');
+
+  document.body.removeChild(copyTextArea);
+  el.attr('data-original-title', elOriginalText);
+}
