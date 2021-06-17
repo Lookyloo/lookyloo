e6a027c82d50531a9b0e4bf9b28ab41a554a9da3
Raphaël Vinot <raphael@vinot.info>
2021-06-16 17:52:27 -0700

    fix: Typing in SRI generator

--- tools/generate_sri.py
+++ tools/generate_sri.py
@@ -5,16 +5,18 @@ import base64
 import hashlib
 import json
 
+from typing import Dict
+
 from lookyloo.helpers import get_homedir
 
 if __name__ == '__main__':
     dest_dir = get_homedir() / 'website' / 'web'
 
-    to_save = {'static': {}}
+    to_save: Dict = {'static': {}}
 
     for resource in (dest_dir / 'static').glob('*'):
         with resource.open('rb') as f:
             to_save['static'][resource.name] = base64.b64encode(hashlib.sha512(f.read()).digest()).decode('utf-8')
 
-    with (dest_dir / 'sri.txt').open('w') as f:
-        json.dump(to_save, f, indent=2)
+    with (dest_dir / 'sri.txt').open('w') as fw:
+        json.dump(to_save, fw, indent=2)
