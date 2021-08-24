c732e38395cc694196b8e11c16d6a6a2f3fc2bdc
Raphaël Vinot <raphael@vinot.info>
2021-08-24 18:44:00 +0200

    chg: Add logging in BG processing

--- bin/background_processing.py
+++ bin/background_processing.py
@@ -37,10 +37,13 @@ class Processing(AbstractManager):
         safe_create_dir(self_generated_ua_file_path)
         self_generated_ua_file = self_generated_ua_file_path / f'{yesterday.isoformat()}.json'
         if self_generated_ua_file.exists():
+            self.logger.info('User-agent file for {yesterday} already exists.')
             return
+        self.logger.info(f'Generating user-agent file for {yesterday}')
         redis = Redis(unix_socket_path=get_socket_path('cache'), decode_responses=True)
         entries = redis.zrevrange(f'user_agents|{yesterday.isoformat()}', 0, -1)
         if not entries:
+            self.logger.info(f'No User-agent file for {yesterday} to generate.')
             return
 
         to_store: Dict[str, Any] = {'by_frequency': []}
@@ -62,6 +65,7 @@ class Processing(AbstractManager):
 
         # Remove the UA / IP mapping.
         redis.delete(f'user_agents|{yesterday.isoformat()}')
+        self.logger.info(f'User-agent file for {yesterday} generated.')
 
 
 def main():
