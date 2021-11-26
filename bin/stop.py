a55fb5380a6d56038e55dc0748d5587fff5c083d
Raphaël Vinot <raphael@vinot.info>
2021-11-26 14:16:22 -0500

    chg: Sync stop script with template

--- bin/stop.py
+++ bin/stop.py
@@ -4,6 +4,7 @@
 from subprocess import Popen, run
 
 from redis import Redis
+from redis.exceptions import ConnectionError
 
 from lookyloo.default import get_homedir, get_socket_path
 
@@ -12,14 +13,18 @@ def main():
     get_homedir()
     p = Popen(['shutdown'])
     p.wait()
-    r = Redis(unix_socket_path=get_socket_path('cache'), db=1)
-    r.delete('shutdown')
-    r = Redis(unix_socket_path=get_socket_path('cache'))
-    r.delete('tree_cache')
-    print('Shutting down databases...')
-    p = run(['run_backend', '--stop'])
-    p.check_returncode()
-    print('done.')
+    try:
+        r = Redis(unix_socket_path=get_socket_path('cache'), db=1)
+        r.delete('shutdown')
+        r = Redis(unix_socket_path=get_socket_path('cache'))
+        r.delete('tree_cache')
+        print('Shutting down databases...')
+        p_backend = run(['run_backend', '--stop'])
+        p_backend.check_returncode()
+        print('done.')
+    except ConnectionError:
+        # Already down, skip the stacktrace
+        pass
 
 
 if __name__ == '__main__':
