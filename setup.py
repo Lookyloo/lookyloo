82a599d8a9f79ad1c80659735b374aa970223db9
Raphaël Vinot <raphael@vinot.info>
2022-03-31 12:31:55 +0200

    chg: Bump deps, version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='lookyloo',
-    version='1.11-dev',
+    version='1.12-dev',
     author='Raphaël Vinot',
     author_email='raphael.vinot@circl.lu',
     maintainer='Raphaël Vinot',
