7c99e7090de9b53528cce0192d3d9cc70623dbd6
Raphaël Vinot <raphael@vinot.info>
2025-03-12 16:37:11 +0100

    chg: Improve kvrocks start script.

--- full_index/run_kvrocks.sh
+++ full_index/run_kvrocks.sh
@@ -3,10 +3,13 @@
 set -e
 set -x
 
-if [ -f ../../kvrocks/build/kvrocks ]; then
+if [ -f ../../kvrocks/build/kvrockss ]; then
     ../../kvrocks/build/kvrocks -c kvrocks.conf
-else
-    echo 'kvrocks does not seem to be installed locally, using the system-wide install instead.'
-    echo 'You can get the DEB package from https://github.com/RocksLabs/kvrocks-fpm/releases'
+elif [ -x "$(command -v kvrocks)" ]; then
+    echo 'kvrocks does not seem to be built locally, using the system-wide install instead.'
     kvrocks -c kvrocks.conf
+else
+    echo 'kvrocks does not seem to be installed, please install kvrocks and try again.'
+    echo 'You can get the DEB package from https://github.com/RocksLabs/kvrocks-fpm/releases'
+    exit 1
 fi
