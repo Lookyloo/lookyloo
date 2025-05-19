44c9ba330b7d7c2250f751a2cdb1bca2aa2acd7b
Raphaël Vinot <raphael@vinot.info>
2025-05-19 15:46:36 +0200

    chg: Bump datatables

--- tools/3rdparty.py
+++ tools/3rdparty.py
@@ -6,9 +6,9 @@ from lookyloo.default import get_homedir
 
 d3js_version = '7.9.0'
 jquery_version = "3.7.1"
-datatables_version = "2.2.2"
+datatables_version = "2.3.1"
 datatables_rowgroup_version = "1.5.1"
-datatables_buttons_version = "3.2.2"
+datatables_buttons_version = "3.2.3"
 datatables_select_version = "3.0.0"
 
 if __name__ == '__main__':
