2a2c1ebc2cf40c4e523fe11000bf7135ef728049
Raphaël Vinot <raphael@vinot.info>
2025-06-16 18:20:43 +0200

    new: Add proxy support to the bot Mastodon.

--- bin/mastobot.py
+++ bin/mastobot.py
@@ -24,16 +24,82 @@ class LookylooMastobotListener(StreamListener):
     def __init__(self, mastobot: Mastobot) -> None:
         self.mastobot = mastobot
         self.blocklist = self.mastobot.config.get('blocklist', [])
+        self.proxies: list[str] = []
         # Avoid loops
         self.blocklist.append(f"{self.mastobot.config['botname']}@{self.mastobot.config['domain']}")
 
     def handle_heartbeat(self) -> None:
         self.mastobot.logger.debug("Heartbeat received")
+        if not self.mastobot.lookyloo.is_up:
+            self.mastobot.logger.error("Lookyloo is not reachable")
+            return
+
+        # get the list of proxies available in the default remote lacus instance
+        if remote_lacuses := self.mastobot.lookyloo.get_remote_lacuses():
+            if isinstance(remote_lacuses, list):
+                # We have more than one remote lacuses, get the default one
+                for remote_lacus in remote_lacuses:
+                    if (remote_lacus.get('is_up')
+                            and remote_lacus.get('name') == self.mastobot.default_remote_lacus):
+                        if proxies := remote_lacus.get('proxies'):
+                            self.proxies = proxies.keys()
+                            break
+                        else:
+                            self.mastobot.logger.info(f"No proxies available in {self.mastobot.default_remote_lacus}")
+                            return
+            else:
+                if remote_lacuses.get('is_up'):
+                    # We have only one remote lacuse, we will use it
+                    if proxies := remote_lacuses.get('proxies'):
+                        self.proxies = proxies.keys()
+        if not self.proxies:
+            self.mastobot.logger.info("No proxies available")
+            return
+
+        note = "Message me one or more URL(s), and I'll capture the page for you. \n \
+                Go to the website for more capture settings."
+
+        # Annoyingly enough, we **must** set all the fields even if we only want to update one of them.
+        # And on top of that, we cannot just use the existing field as if it is a URL,
+        # it will have been escaped, and we're going to re-escape it which will break the field.
+        # Each field bust be set here.
+        # The entries we have are:
+        # 1. Public URL of he Lookyloo instance
+        # 2. Proxies available for capturing
+        # 3. Query format for the bot
+        # 4. The repository of the project
+        # Only trigger the update if the proxies have changed
+        account_details = self.mastobot.mastodon.me()
+        proxy_field_exists = False
+        proxies_changed = False
+        proxies_str = ', '.join(self.proxies)
+        fields_to_submit = []
+        if account_details.fields:
+            for field in account_details.fields:
+                if field['name'] == 'Proxies':
+                    proxy_field_exists = True
+                    if field['value'] != proxies_str:
+                        proxies_changed = True
+                        if proxies_str:
+                            # Update the field with the list of proxies
+                            fields_to_submit.append(("Proxies", proxies_str))
+            if not proxy_field_exists:
+                # Add the proxies field
+                proxies_changed = True
+                fields_to_submit.append(("Proxies", proxies_str))
+        if proxies_changed:
+            self.mastobot.logger.info("Proxies have changed, update the account fields")
+            fields_to_submit.insert(0, ("Website", self.mastobot.lookyloo.root_url))
+            fields_to_submit.insert(2, ("Query format (single URL only)", '(<Optional_Proxy_Name>) <URL>'))
+            fields_to_submit.insert(3, ("Repository", "https://github.com/Lookyloo"))
+            self.mastobot.mastodon.account_update_credentials(note=note, fields=fields_to_submit)
+        else:
+            self.mastobot.logger.debug("Proxies have not changed, no need to update the account fields")
 
     def on_update(self, status: Status) -> None:
         self.mastobot.logger.debug(f"Update: {status}")
 
-    def _find_url(self, content: str) -> list[str]:
+    def _find_url(self, content: str) -> list[str] | list[tuple[str, str]]:
         # Case 1, the toot has 2 words, the first is the username, the second is the URL
         doc = html.document_fromstring(content)
         body = doc.text_content().strip()
@@ -41,6 +107,9 @@ class LookylooMastobotListener(StreamListener):
         if len(splitted) == 2:
             # The first word is the username, the rest is the URL
             return [splitted[1]]
+        elif len(splitted) == 3 and splitted[1] in self.proxies:
+            # The first word is the username, the second is the proxy, the third is the URL
+            return [(splitted[2], splitted[1])]
 
         # Case 2: we get all the hyperlinks in the toot (except the ones pointing to users)
         to_return = []
@@ -78,16 +147,27 @@ class LookylooMastobotListener(StreamListener):
                         return
                 visibility = notification['status']['visibility']
                 spoiler_text = notification['status']['spoiler_text']
-                for url in self._find_url(notification['status']['content']):
-                    self.mastobot.logger.info(f"URL: {url}")
+                for _url in self._find_url(notification['status']['content']):
+                    if isinstance(_url, tuple):
+                        # We have a tuple, the first element is the URL, the second is the proxy
+                        url, proxy = _url
+                        self.mastobot.logger.info(f"Using proxy {proxy} for {url}")
+                    else:
+                        # We just have a URL
+                        url = _url
+                        proxy = None
+                        self.mastobot.logger.info(f"URL: {url}")
                     if not url:
                         continue
                     try:
-                        permaurl = self.mastobot.lookyloo.submit(url=url)
+                        permaurl = self.mastobot.lookyloo.submit(url=url, proxy=proxy)
                     except Exception as error:
                         self.mastobot.logger.error(f"Error while submitting {url}: {error}")
                         return
-                    text = f'@{sender} Here is your capture of {defang(url)}: {permaurl}\n It may take a minute to complete, please be patient. #bot'
+                    text = f'@{sender} Here is your capture of {defang(url)}: {permaurl}'
+                    if proxy:
+                        text += f' (using proxy: {proxy}).'
+                    text += '\n It may take a minute to complete, please be patient. #bot'
                     self.mastobot.mastodon.status_post(text, in_reply_to_id=status_id, visibility=visibility, spoiler_text=spoiler_text)
             else:
                 self.mastobot.logger.debug(f"Unhandled notification type: {notification['type']}")
@@ -117,20 +197,27 @@ class Mastobot(AbstractManager):
             self.logger.info("Mastobot is disabled, aborting.")
             return
 
-        self.logger.setLevel(self.config.get('loglevel', 'DEBUG'))
+        self.logger.setLevel(self.config.get('loglevel', 'INFO'))
 
-        self.mastodon = Mastodon(api_base_url=self.config['domain'],
+        lookyloo_url = get_config('generic', 'public_domain') if not self.config.get('remote_lookyloo') else self.config.get('remote_lookyloo')
+        self.lookyloo = PyLookyloo(lookyloo_url)
+        if not self.lookyloo.is_up:
+            self.logger.error("Lookyloo is not reachable, aborting.")
+            return
+
+        if get_config('generic', 'multiple_remote_lacus').get('enable'):
+            # Multiple remote lacus are enabled, we will use the default one for the proxies
+            self.default_remote_lacus = get_config('generic', 'multiple_remote_lacus').get('default')
+        else:
+            self.default_remote_lacus = 'default'
+
+        self.mastodon = Mastodon(api_base_url=f"https://{self.config['domain']}",
                                  access_token=self.config['access_token'],
                                  debug_requests=False)
         try:
             self.mastodon.account_verify_credentials()
         except MastodonError as e:
             self.logger.error(f"Error while verifying credentials: {e}")
-            return
-        lookyloo_url = get_config('generic', 'public_domain') if not self.config.get('remote_lookyloo') else self.config.get('remote_lookyloo')
-        self.lookyloo = PyLookyloo(lookyloo_url)
-        if not self.lookyloo.is_up:
-            self.logger.error("Lookyloo is not reachable, aborting.")
             return
 
         if not self.mastodon.stream_healthy():
