af173922acdb78bc9ea097bee48b62bd5f5a28e7
Raphaël Vinot <raphael@vinot.info>
2025-10-13 15:05:57 +0200

    fix: Make mypy happy

--- tests/test_generic.py
+++ tests/test_generic.py
@@ -4,14 +4,14 @@ import re
 from playwright.sync_api import Page, expect
 
 
-def test_has_title(page: Page):
+def test_has_title(page: Page) -> None:
     page.goto("http://127.0.0.1:5100/index")
 
     # Expect a title "to contain" a substring.
     expect(page).to_have_title(re.compile("Lookyloo"))
 
 
-def test_get_started_link(page: Page):
+def test_get_started_link(page: Page) -> None:
     page.goto("http://127.0.0.1:5100/index")
 
     page.get_by_role("link", name="Start a new capture").click()
