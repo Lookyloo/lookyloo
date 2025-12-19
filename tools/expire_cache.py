b84d42286a7c6bce14a019ad55fe49ca924e83ea
Raphaël Vinot <raphael@vinot.info>
2025-12-19 18:24:38 +0100

    fix: incorrect stats for recent captures due to early expiring

--- tools/expire_cache.py
+++ tools/expire_cache.py
@@ -5,13 +5,16 @@ from datetime import timedelta
 from redis import Redis
 
 from lookyloo.default import get_socket_path, get_config
+from lookyloo import Lookyloo
 
 redis_cache = Redis(unix_socket_path=get_socket_path('cache'), decode_responses=True)
 
-time_delta_on_index = timedelta(**get_config('generic', 'time_delta_on_index'))
+time_delta_on_index = timedelta(days=get_config('generic', 'archive'))
 
-for uuid, capture_dir in redis_cache.hscan_iter('lookup_dirs'):
-    redis_cache.expire(capture_dir, int(time_delta_on_index.total_seconds()) * 2)
+lookyloo = Lookyloo()
+
+for cc in lookyloo.sorted_capture_cache(cached_captures_only=False):
+    redis_cache.expire(str(cc.capture_dir), int(time_delta_on_index.total_seconds()) * 2)
 
 
 for uuid, capture_dir in redis_cache.hscan_iter('lookup_dirs_archived'):
